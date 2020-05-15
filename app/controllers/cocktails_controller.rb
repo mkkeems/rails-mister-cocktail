027ff469016b7881e4ead8ff5eadf2841ba5548d
Michelle Kim <mkkeems@gmail.com>
2020-05-15 16:04:39 +0900

    can add image url and edit single cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,10 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
@@ -22,8 +22,17 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  # PATCH  patch the updated article
+  def update
+    @cocktail.update(cocktail_params)
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
   def destroy
-    @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
     # no need for app/views/cocktails/destroy.html.erb
     redirect_to cocktails_path
@@ -31,7 +40,11 @@ class CocktailsController < ApplicationController
 
   private
 
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :imgurl)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :create, :new, :destroy] do
+  resources :cocktails do
     resources :doses, only: [:create, :new]
   end
   resources :doses, only: [:destroy]
